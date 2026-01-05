f7c91aa04a8ce8949ed6d44c3b38c785b59b0212
theHimanshuShekhar <hshekhar.dev@gmail.com>
2026-01-06 01:41:22 +0530

    feat: update Header component with icon support and remove ThemeProvider

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,7 @@
 import { Link } from "@tanstack/react-router"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { HugeiconsIcon } from '@hugeicons/react';
+import { GameController03FreeIcons } from '@hugeicons/core-free-icons';
 
 interface HeaderProps {
   title?: string
@@ -9,12 +11,17 @@ export function Header({ title = "Pinger!" }: HeaderProps) {
   return (
     <header className="w-full border-b border-muted bg-background/50 backdrop-blur-sm">
       <div className="mx-auto flex justify-between h-14 max-w-7xl items-center gap-4 px-4">
-        <Link  to="/" className="flex items-center gap-3">
+        <Link to="/" className="flex items-center gap-1">
+          <HugeiconsIcon
+            icon={GameController03FreeIcons}
+            strokeWidth={2}
+            className="size-8 text-primary"
+          />
           <div className="text-3xl font-semibold">{title}</div>
         </Link>
 
         <div className="flex items-center gap-2">
-           <Avatar className="size-12">
+          <Avatar className="size-10">
             <AvatarImage
               src="https://github.com/shadcn.png"
             />
